b3a4bafb2cb61769a9ce45dbc70bc3bfaf885570
Jonathan Jiménez <tj0027032023@unab.edu.sv>
2024-08-30 14:40:15 -0600

    .

--- Cadenaconexion/Conexion.cs
+++ Cadenaconexion/Conexion.cs
@@ -8,13 +8,20 @@ using System.Data.SqlClient;
 
 namespace Cadenaconexion
 {
+    // Esta clase maneja la conexión a la base de datos.
     internal class Conexion
     {
+        // Este método establece y devuelve una conexión con la base de datos.
         public static SqlConnection Conectar()
         {
+
+            // Se crea una nueva conexión usando una cadena de conexión específica.
             SqlConnection cn = new SqlConnection("Data Source=LAPTOP-T37BFAOL\\SQLEXPRESS;Initial Catalog=Northwind;Integrated Security=True;");
-            cn .Open();
+            // Se abre la conexión a la base de datos.
+            cn.Open();
+            // Se retorna el objeto de conexión para ser usado en otras partes del programa.
             return cn;
+
         }
 
     }
--- Cadenaconexion/Program.cs
+++ Cadenaconexion/Program.cs
@@ -6,16 +6,23 @@ using System.Windows.Forms;
 
 namespace Cadenaconexion
 {
+    // Clase principal donde comienza la ejecución del programa.
     internal static class Program
     {
         /// <summary>
         /// Punto de entrada principal para la aplicación.
         /// </summary>
         [STAThread]
+        // Método principal que se ejecuta cuando se inicia la aplicación.
         static void Main()
         {
+            // Habilita estilos visuales para los controles de Windows Forms.
             Application.EnableVisualStyles();
+
+            // Configura la forma en que se renderiza el texto para que sea compatible.
             Application.SetCompatibleTextRenderingDefault(false);
+
+            // Inicia y muestra el formulario principal de la aplicación (Form1).
             Application.Run(new Form1());
         }
     }
